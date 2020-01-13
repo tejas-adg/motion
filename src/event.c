67fdd39e4afb4c41cf5f450e22a34d62052f2656
Tejas Adagoor Manjunatha <tejasam@vt.edu>
2020-01-12 19:25:13 -0600

    executes the python script

--- src/event.c
+++ src/event.c
@@ -93,7 +93,7 @@ static void exec_command(struct context *cnt, char *command, char *filename, int
             close(i);
 
         //execl("/bin/sh", "sh", "-c", stamp, " &", NULL);
-        execl("/usr/bin/python3", "", "/home/pi/Documents/hello.py", NULL);
+        execl("/usr/bin/python3", "python3", "/home/pi/Documents/emailer.py", NULL);
 
         /* if above function succeeds the program never reach here */
         MOTION_LOG(ALR, TYPE_EVENTS, SHOW_ERRNO
